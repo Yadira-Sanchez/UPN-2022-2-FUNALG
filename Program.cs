af42d47f9135cc1071b5b1e5c7b31dcd48f235ec
Yadira-Sanchez <114895925+Yadira-Sanchez@users.noreply.github.com>
2022-10-06 23:01:50 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// 8. Diseñar un programa que permita escribir en una pantalla la frase
+// “¿Desea continuar? S/N” hasta que la respuesta sea 'N'
+
+string caracter;
+do
+{
+    Console.Write("¿DESEA CONTINUAR? S/N: ");
+    caracter = Console.ReadLine();
+}
+while (caracter != "N");

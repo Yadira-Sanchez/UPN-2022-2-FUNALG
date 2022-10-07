d6a1cd11a971a81acab61d32f7eb233c5c24eb76
Yadira-Sanchez <114895925+Yadira-Sanchez@users.noreply.github.com>
2022-10-06 22:58:38 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// 5. Diseñar un programa para imprimir la suma de los primeros números naturales impares menores o iguales que n.
+
+int num, suma = 0;
+
+Console.WriteLine("ESCRIBA UN NUMERO: ");
+
+num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i = i + 2)
+{
+    suma = suma + i;
+}
+Console.WriteLine("LA SUMA DE NUMEROS IMPARES ES : " + suma);
+
+Console.ReadKey();

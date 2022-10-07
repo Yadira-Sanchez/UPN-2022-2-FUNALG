6e4b713f90caa6a19ef9e67448cc640a8c6db507
Yadira-Sanchez <114895925+Yadira-Sanchez@users.noreply.github.com>
2022-10-06 23:00:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// 7. Diseñar un programa para buscar y escribir la primera vocal leída del teclado.
+// (Se supone que se leen, uno a uno, caracteres desde el teclado.)
+
+char caracter;
+do
+{
+    Console.Write("INGRESE UNA LETRA: ");
+    caracter = char.Parse(Console.ReadLine());
+}
+while (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u' && caracter != 'A');
+
+
+Console.WriteLine("LA PRIMERA VOCAL INGRESADA ES: " + caracter);

19d96e00221ef999d2ac386337c92e9361f3b872
Yadira-Sanchez <114895925+Yadira-Sanchez@users.noreply.github.com>
2022-10-06 23:02:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// 9. Diseñar un programa para leer sucesivamente números del teclado hasta que aparezca un número comprendido entre 1 y 5.
+
+int num;
+do
+{
+    Console.Write("\t\tINGRESE UN NUMERO: ");
+    num = int.Parse(Console.ReadLine());
+} while (num <= 1 || num >= 5);

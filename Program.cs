9c12de28892f31ca84010a6ab12255eca7232b16
Yadira-Sanchez <114895925+Yadira-Sanchez@users.noreply.github.com>
2022-10-06 22:59:38 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//6. Diseñar un programa para leer una serie de números hasta obtener un número superior a 100.
+
+int numero;
+do
+{
+    Console.Write("INGRESE UN NUMERO SEGUID0 DE ENTER: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+while (numero <= 100);
+
+Console.Write("ERROR!!! NUMERO FUERA DE RANGO: ");

756a8a900a497b3c53eaa5f795d2e94173f9d227
Yadira-Sanchez <114895925+Yadira-Sanchez@users.noreply.github.com>
2022-10-06 22:56:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// 4. Diseñar un programa para calcular la suma de los números naturales,
+// comprendidos entre 2 números ingresados por el usuario.
+
+
+int num1, num2, suma = 0;
+
+Console.WriteLine("INGRESE EL PRIMER NUMERO ");
+
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL SEGUNDO NUMERO");
+
+num2 = int.Parse(Console.ReadLine());
+
+for (int i = num1; i <= num2; i++)
+{
+    suma = suma + i;
+}
+
+Console.WriteLine("LA SUMA COMPRENDIDOS ENTRE ESTOS DOS NUMEROS ES: : " + suma);
+
+Console.ReadKey();

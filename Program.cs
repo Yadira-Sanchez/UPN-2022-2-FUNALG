c989848a4df79557ae6f3157eed1be1f3fa5c365
Yadira-Sanchez <114895925+Yadira-Sanchez@users.noreply.github.com>
2022-10-06 22:35:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// 1. Diseñar un programa para imprimir todos los números primos entre 2 y 100 inclusive.
+
+int dividendo = 2;
+int divisor = 1;
+int divisiones = 0;
+
+while (dividendo <= 100)
+{
+    while (dividendo <= 100)
+    {
+        while (divisor <= dividendo)
+        {
+            if (dividendo % divisor == 0)
+            {
+                divisiones++;
+            }
+            divisor++;
+        }
+
+        if (divisiones == 2)
+        {
+            Console.WriteLine(dividendo);
+        }
+
+        dividendo++;
+        divisor = 1;
+        divisiones = 0;
+    }
+}
